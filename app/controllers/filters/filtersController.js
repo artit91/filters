ae0a75e885bfdf3cddcbcc862dc57c804dd0b8f9
Attila Varga <artit91@gmail.com>
2015-11-28 21:15:01 +0000

    css to caman

--- app/controllers/filters/filtersController.js
+++ app/controllers/filters/filtersController.js
@@ -1,4 +1,3 @@
-
 /*global filters, swal, escape */
 filters.controller('filtersController', [
     'utils',
@@ -7,6 +6,34 @@ filters.controller('filtersController', [
     '$scope',
     '$timeout',
     function (utils, $stateParams, $state, $scope, $timeout) {
+        function contrast (cssArg) {
+            return Math.round(
+                cssArg < 1 ? (1 - cssArg) * -100 : (cssArg - 1) * 100
+            );
+        }
+
+        function saturate (cssArg) {
+            return Math.round(
+                cssArg < 1 ? (1 - cssArg) * -100 : (cssArg - 1) * 100
+            );
+        }
+
+        function sepia (cssArg) {
+            return Math.round(cssArg * 100);
+        }
+
+        function brightness (cssArg) {
+            return Math.round(
+                cssArg < 1 ? (1 - cssArg) * -100 : (cssArg - 1) * 100
+            );
+        }
+
+        function hueRotate (cssArg) {
+            return Math.round(
+                (((360 + cssArg) % 360) / 360) * 100
+            );
+        }
+
         $scope.selected = 'default';
         $scope.photoTaken = 0;
         $scope.noCamera = 0;
@@ -17,19 +44,39 @@ filters.controller('filtersController', [
                 'name': 'default'
             },
             {
-                'name': 'OpenMayfair'
+                'name': 'OpenMayfair',
+                'contrast': contrast(1.1),
+                'saturation': saturate(1.4)
             },
             {
-                'name': 'OpenRise'
+                'name': 'OpenRise',
+                'contrast': contrast(0.8),
+                'saturation': saturate(1.4),
+                'sepia': sepia(0.25),
+                'brightness': brightness(1.1),
+                'hue': hueRotate(-15)
             },
             {
-                'name': 'OpenValencia'
+                'name': 'OpenValencia',
+                'contrast': contrast(0.9),
+                'saturation': saturate(1.5),
+                'sepia': sepia(0.15)
             },
             {
-                'name': 'OpenHefe'
+                'name': 'OpenHefe',
+                'contrast': contrast(1.3),
+                'saturation': saturate(1.3),
+                'sepia': sepia(0.30),
+                'brightness': brightness(0.95),
+                'hue': hueRotate(-10)
             },
             {
-                'name': 'OpenNashville'
+                'name': 'OpenNashville',
+                'contrast': contrast(0.9),
+                'saturation': saturate(1.5),
+                'sepia': sepia(0.40),
+                'brightness': brightness(1.1),
+                'hue': hueRotate(-15)
             }
         ];
 
