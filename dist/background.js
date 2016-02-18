61f9f0ef8ace3a10b77ed633df65ce3f00901e99
Attila Varga <artit91@gmail.com>
2016-02-18 18:58:57 +0000

    project setup refactor

---
+++ dist/background.js
@@ -0,0 +1,1 @@
+function init(){chrome.app.runtime.onLaunched.addListener(function(){chrome.app.window.create("index.html",{id:"filters",innerBounds:{width:533,height:400,minWidth:533,minHeight:400}})})}init();
